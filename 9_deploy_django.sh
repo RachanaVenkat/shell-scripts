09119d17c6eb5df98fc5ef2dc01078a5b7d72421
Rachana <151712438+RachanaVenkat@users.noreply.github.com>
2024-08-21 19:40:20 +0530

    Add files via upload

--- 9_deploy_django.sh
+++ 9_deploy_django.sh
@@ -3,6 +3,7 @@
 << task
 Deploy a Django app
 and handle the code for errors
+access using - http://localhost:8000/
 task
 
 code_clone(){
@@ -17,7 +18,7 @@ code_clone(){
 
 install_requirements(){
 	echo "installing dependencies..."
-	if ! sudo dnf install -y --allowerasing docker-ce docker-ce-cli containerd.io nginx; then
+	if ! sudo dnf install -y --allowerasing docker-ce docker-ce-cli containerd.io nginx docker-compose; then
        		 echo "Failed to install dependencies."
        		 exit 1
    	fi
@@ -27,6 +28,7 @@ required_restarts(){
     echo "Enabling and starting services..."
     sudo systemctl enable --now docker || { echo "Failed to start Docker."; exit 1; }
     sudo systemctl enable --now nginx || { echo "Failed to start Nginx."; exit 1; }
+    sudo systemctl restart docker
 }
 
 deploy(){
@@ -35,9 +37,13 @@ deploy(){
         echo "Docker build failed."
         exit 1
     fi
-    if ! docker run -d -p 8000:8000 notes-app:latest; then
-        echo "Failed to run the Docker container."
-        exit 1
+   # if ! docker run -d -p 8000:8000 notes-app:latest; then
+    #    echo "Failed to run the Docker container."
+     #   exit 1
+   # fi
+    if ! docker-compose up -d; then
+	    echo "Failed to run the docker container."
+	    exit 1
     fi
 }
 
